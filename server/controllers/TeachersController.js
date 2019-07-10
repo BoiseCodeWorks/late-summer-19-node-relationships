64fe6e8de1561afa7be31e576a3eea1ebec96eb3
Darryl Kilzer <darrylkilzer@gmail.com>
2019-07-10 13:58:44 -0600

    finished

--- server/controllers/TeachersController.js
+++ server/controllers/TeachersController.js
@@ -17,11 +17,34 @@ export default class TeachersController {
     } catch (err) { next(err) }
   }
 
+  async getTeacher(req, res, next) {
+    try {
+      let teacher = await _teachersService.findById(req.params.teacherId).populate('subject students')
+      res.send(teacher)
+    }
+    catch (err) { next(err) }
+  }
+
+  async addStudent(req, res, next) {
+    try {
+      let teacher = await _teachersService.findById(req.params.teacherId)
+      teacher.students.push(req.body.studentId)
+      await teacher.save(err => {
+        if (err) {
+          next(err)
+        }
+      })
+      res.send(teacher)
+    } catch (err) { next(err) }
+  }
 
 
   constructor() {
     this.router = express.Router()
       .get('', this.getAllTeachers)
+      .get('/:teacherId', this.getTeacher)
       .post('', this.createTeacher)
+      .put('/:teacherId/students/', this.addStudent)
+    // .put('/:teacherId', this.editTeacher)
   }
 }
--- server/services/TeachersService.js
+++ server/services/TeachersService.js
@@ -3,6 +3,7 @@ let ObjectId = mongoose.Schema.Types.ObjectId
 
 let _schema = new mongoose.Schema({
   name: { type: String, required: true },
+  //NOTE one to many, one teacher hs many students
   students: [{ type: ObjectId, ref: 'Student' }],
   subject: { type: ObjectId, ref: "Subject", required: true }
 }, { timestamps: true })
